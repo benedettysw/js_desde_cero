8dd2fdcacad8e5d4b078ba9cf447f371ac44c4e6
benedettysw <benedetty321@gmail.com>
2024-03-09 20:12:35 -0500

    jsbasico

---
+++ Ejercicios.js
@@ -0,0 +1,27 @@
+let comida = 2800 + 1300;
+
+let bebida = 900 + 650 ;
+
+console.log(comida)
+
+console.log(bebida)
+
+console.log(comida + bebida)
+
+
+let partido = false;
+
+if (partido == true){
+
+    console.log("Partido si")
+
+}else{
+
+    console.log("Partido no")
+}
+
+let x = 3 , y ;f
+
+y = x++;
+
+console.log(y +" , "+ x)
---
+++ Inducion.js
@@ -0,0 +1,50 @@
+
+//var permite colocar un nombre de variable que ya existe no uses var
+
+//var es globar -- let y const no ejemplo
+
+if (true){
+
+    let sasludar ="hola careverga"
+
+}
+//Si imprime saludar fuera del conjunto del codigo con si permite a diferencia de let y conts
+
+console.log(sasludar)
+
+
+
+//Operadores aritmeticos
+
+let Resto = 9 % 2;
+
+console.log(Resto)  //Sobrante 0 reciduos
+
+
+//Operadores de comparacion true or false
+
+console.log(10<2)
+
+console.log(4>1)
+
+let numero = 4;
+
+console.log(4 === numero)
+
+
+//Comparacion de logica (&& Y) se debe de cumplir todas las comparaciones si o si
+
+let edad = 10 , dni = true;5
+
+console.log(edad >= 18 && dni == true)
+
+
+//Comparacion de logica (|| OR) se debe de cumplir por lo meno 1 
+
+let edad1 = 10 , dni1 = true;
+
+console.log(edad >= 18 || dni == true)
+
+
+
+
